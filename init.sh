08180cba31810138260137ab104cc766bb0a9fbe
AndrewSamokhvalov <andrew.shvv@gmail.com>
2015-10-08 19:06:36 +0300

    Delete init.sh

--- init.sh
+++
@@ -1,9 +0,0 @@
-# ================= EXAMPLE =====================
-export AWS_KEYNAME="AndreySamokhvalov"
-export AWS_KEYPATH="$HOME/.ssh/AndreySamokhvalov.pem"
-export AWS_KEY="AKIAJA78g76954OsfS2B3UUHQ"
-export AWS_SECRET="e0w01wf+qUgAI48g8p70gCjasd5Q4taXYLGTTLeabOai+Ec5yZ"
-
-export PROJECT_GIT_URL="git@gitlab.com:AndreySamokhvalov/AdvertismentTelegramBot.git"
-export PROJECT_GIT_SSH_KEY_PATH="$HOME/.ssh/id_rsa"
-export PROJECT_DIR="$HOME/../project"
